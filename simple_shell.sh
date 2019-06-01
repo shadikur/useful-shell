58674358a9b803940d2eda88c761e17c8e1bd71a
Shadikur <me@shadikur.com>
2019-06-01 23:56:49 +0000

    Updated version for Debian & Ubuntu only

--- simple_shell.sh
+++ simple_shell.sh
@@ -4,41 +4,48 @@
 # Copyright (c) shadikur.com
 # Script follows here:
 
-echo "Current RAM"
+#All the variables
+bold=$(tput bold)
+normal=$(tput sgr0)
+green=$(tput setaf 2)
+
+echo "${bold}Current RAM${normal}"
 free -m
 #
-echo "System updating ... "
+echo "\n"
+echo "${bold}System updating ... \n"
 apt update -y && apt -y upgrade && apt install build-essential -y && apt install wget git zip unzip vim nano dialog curl lsb-release -y
-echo "System upgrade complete."
+echo  "\n${bold}${green}System upgrade complete.${normal} \n"
 
 #SWAP memory
 echo "For better performance on processing, we can create virtual RAM (SWAP Memory) on your system"
 echo "Do you want to proceed ? For yes Press y/Y  and n/N for NO: "
 read RESPONSE
 
-if [ $RESPONSE =  "y" ];
+if [ ${RESPONSE} =  "y" ];
 	then
 	echo "Enter the memory that you want to make your SWAP in Mega Byte (e.g. 1024)"
 	read MEMORY
+	echo "\n${bold}Processing...Please wait.${normal}\n"
 	cd /var
 	touch swap.img
 	chmod 600 swap.img
 	dd if=/dev/zero of=/var/swap.img bs=${MEMORY}k count=1000
-	echo "SWAP Processed Successfully"
+	echo  "${bold}${green}SWAP Processed Successfully${normal}\n"
 	mkswap /var/swap.img
 	swapon /var/swap.img
 	echo "/var/swap.img    none    swap    sw    0    0" >> /etc/fstab
-	echo "SWAP Memory added successfully."
 	sysctl -w vm.swappiness=30
+	echo  "${bold}${green}SWAP Memory added successfully.${normal}\n"
 else 
-	echo "Sorry SWAP created aborted"
+	echo "${bold}Sorry SWAP created aborted${normal}"
 fi
 
 #
-echo "Updated RAM and SWAP"
+echo "\n${bold}Updated RAM and SWAP${normal}"
 free -m
 
-echo "System Disk Status"
+echo  "\n${bold}System Disk Status${normal}"
 df -h
 
 
