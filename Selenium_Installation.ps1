d346951cd44804c50a3ec8cb5d4fbea19b93d47f
M S Rahman <me@shadikur.com>
2022-06-14 07:05:51 +0200

    Update Selenium_Installation.ps1

--- Selenium_Installation.ps1
+++ Selenium_Installation.ps1
@@ -2,6 +2,7 @@
 #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 $source = 'https://github.com/SeleniumHQ/selenium/releases/download/selenium-3.141.59/selenium-java-3.141.59.zip'
 $chromeDriver = 'https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_win32.zip'
+$javaJDK = 'http://java.a2zlab.online/s/B9ffYZSHz9SYNBS/download/jdk-11.0.15_windows-x64_bin.exe'
 $destination = 'c:\java'
 $driverPath = 'c:\java\driver'
 $filePath = 'c:\java\selenium-java-3.141.59.zip'
@@ -12,6 +13,10 @@ else {
     mkdir $driverPath
     Write-Host "Java folder was not there, so it has been created now." -ForegroundColor Black -BackgroundColor White
 }
+Write-Host "Downloading Java ... " -ForegroundColor Yellow -BackgroundColor Black
+Invoke-WebRequest -Uri $java -OutFile $destination
+Start-Sleep 2
+
 Write-Host "Downloading Selenium ... " -ForegroundColor Yellow -BackgroundColor Black
 Invoke-WebRequest -Uri $source -OutFile $filePath
 Start-Sleep 2
@@ -25,6 +30,7 @@ Expand-Archive -LiteralPath $destination\chromedriver_win32.zip -DestinationPath
 Start-Sleep 1
 Write-Host "Cleaning temp files..."
 Remove-Item -Path $destination\*.zip*
+Remove-Item -Path $destination\jdk-11.0.15_windows-x64_bin.exe
 Remove-Item -Path $driverPath\*.zip*
 Start-Sleep 1
 Write-Host "Completed, you can configure selenium from eclips now `n`n" -ForegroundColor Yellow -BackgroundColor Black
