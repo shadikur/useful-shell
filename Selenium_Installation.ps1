8c0459ecaad083c9cc9e6d234f58cc4c2a69db0b
M S Rahman <shadikurbd@gmail.com>
2022-06-14 05:51:04 +0200

    Add files via upload

---
+++ Selenium_Installation.ps1
@@ -0,0 +1,29 @@
+﻿#!ps
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+$source = 'https://github.com/SeleniumHQ/selenium/releases/download/selenium-3.141.59/selenium-java-3.141.59.zip'
+$chromeDrive = 'https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_win32.zip'
+$destination = 'c:\java\driver'
+$filePath = 'c:\java\driver\selenium-java-3.141.59.zip'
+if (Test-Path -Path $destination) {
+    Write-Host "Temp folder already exists, proceeding to next step" -ForegroundColor Black -BackgroundColor White
+}
+else {
+    mkdir $destination
+    Write-Host "Temp folder was not there, so we have created just now." -ForegroundColor Black -BackgroundColor White
+}
+Write-Host "Downloading Selenium ... " -ForegroundColor Yellow -BackgroundColor Black
+Invoke-WebRequest -Uri $source -OutFile $filePath
+Start-Sleep 2
+Write-Host "Downloading ChromeDriver ... " -ForegroundColor Yellow -BackgroundColor Black
+Invoke-WebRequest -Uri $chromeDrive -OutFile $destination\chromedriver_win32.zip
+Start-Sleep 3
+Write-Host "Download has been completed, extracting the file now." -ForegroundColor Black -BackgroundColor White
+Start-Sleep 2
+Expand-Archive -LiteralPath $filePath -DestinationPath $destination\slenium-java
+Expand-Archive -LiteralPath $destination\chromedriver_win32.zip -DestinationPath $destination\
+Start-Sleep 1
+Write-Host "Cleaning temp files..."
+Remove-Item -Path $destination\*.zip*
+Start-Sleep 1
+Write-Host "Completed, you can configure selenium from eclips now `n`n" -ForegroundColor Yellow -BackgroundColor Black
+Write-Host "Absolute path for ChromeDriver: $destination\chromedriver.exe `n" -ForegroundColor DarkGreen -BackgroundColor Black
