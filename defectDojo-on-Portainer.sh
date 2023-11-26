26fa0a3964b7e022196e3ebf83046cb3568a6721
M S Rahman <me@shadikur.com>
2023-11-26 22:01:57 +0100

    Update defectDojo-on-Portainer.sh

--- defectDojo-on-Portainer.sh
+++ defectDojo-on-Portainer.sh
@@ -24,6 +24,12 @@ DESTINATION=/usr/local/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
 sudo chmod 755 $DESTINATION
 
+# Update docker compose plugin
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
+
 # Verify the installation
 docker-compose --version
 
